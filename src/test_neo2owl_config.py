0fe045d500df3432c6330db09087b38119094e65
Ismail Ugur Bayindir <ubyndr@gmail.com>
2023-04-19 18:17:44 +0100

    Update test_neo2owl_config.py

--- src/test_neo2owl_config.py
+++ src/test_neo2owl_config.py
@@ -2,4 +2,4 @@ from utils.schema_test_tools import test_local
 
 test_local(path_to_schema_dir='config/dumps/',
            schema_file='neo4j2owl_config_schema.json',
-           path_to_test_dir='config/prod/')
+           path_to_test_dir='config/dumps/')
